4b3145aac0480f343d3b6445d2aa5e47b939a1af
sudo <ykd@asia.com>
2025-11-16 08:08:57 -0500

    Add client-side screen and renderer

---
+++ src/main/java/com/yourname/companionmod/screen/CompanionScreen.java
@@ -0,0 +1,34 @@
+package com.yourname.companionmod.screen;
+
+import com.yourname.companionmod.CompanionMod;
+import com.yourname.companionmod.menu.CompanionMenu;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+
+public class CompanionScreen extends AbstractContainerScreen<CompanionMenu> {
+    private static final ResourceLocation TEXTURE = 
+        ResourceLocation.fromNamespaceAndPath(CompanionMod.MOD_ID, "textures/gui/companion_inventory.png");
+
+    public CompanionScreen(CompanionMenu menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+        this.imageHeight = 168;
+        this.inventoryLabelY = this.imageHeight - 94;
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+        int x = (this.width - this.imageWidth) / 2;
+        int y = (this.height - this.imageHeight) / 2;
+        guiGraphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight);
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        this.renderTooltip(guiGraphics, mouseX, mouseY);
+    }
+}
