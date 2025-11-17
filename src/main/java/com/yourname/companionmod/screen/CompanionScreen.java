4340363eea797c530f0d821227616d16eb883720
sudo <ykd@asia.com>
2025-11-17 06:28:07 -0500

    fix: update to ResourceLocation.fromNamespaceAndPath and new renderEntityInInventory signature for Minecraft 1.21.x/NeoForge

--- src/main/java/com/yourname/companionmod/screen/CompanionScreen.java
+++ src/main/java/com/yourname/companionmod/screen/CompanionScreen.java
@@ -2,7 +2,6 @@ package com.yourname.companionmod.screen;
 
 import com.yourname.companionmod.entity.custom.CompanionEntity;
 import com.yourname.companionmod.menu.CompanionMenu;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -10,10 +9,12 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
+import org.joml.Vector3f;
+import org.joml.Quaternionf;
 
 public class CompanionScreen extends AbstractContainerScreen<CompanionMenu> {
     private static final ResourceLocation SURVIVAL_TEXTURE =
-        new ResourceLocation("minecraft", "textures/gui/container/inventory.png");
+        ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/container/inventory.png");
 
     public CompanionScreen(CompanionMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
@@ -33,16 +34,20 @@ public class CompanionScreen extends AbstractContainerScreen<CompanionMenu> {
         guiGraphics.blit(SURVIVAL_TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight);
 
         // Fill the crafting grid + result slot area with inventory background
-        // (Vanilla crafting grid: 98, 18 - 55x55 px area)
         int bgColor = 0xFF373737; // Sampled from vanilla background
         guiGraphics.fill(x + 98, y + 18, x + 98 + 55, y + 18 + 55, bgColor);
 
         // Render the companion entity (in the spot the player would normally be)
         if (this.menu.getCompanion() != null) {
             LivingEntity companion = this.menu.getCompanion();
-            InventoryScreen.renderEntityInInventory(x + 51, y + 75, 30,
-                (float)(x + 51) - mouseX,
-                (float)(y + 75 - 50) - mouseY, companion);
+            InventoryScreen.renderEntityInInventory(
+                guiGraphics,
+                (float)(x + 51), (float)(y + 75), 30f,
+                new Vector3f(0f, 0f, 0f),
+                new Quaternionf().rotationXYZ(0, 0, 0),
+                new Quaternionf().rotationY((float)Math.PI),
+                companion
+            );
         }
     }
 
